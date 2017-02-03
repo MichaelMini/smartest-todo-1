6c0e75bbdcbf74e0cd017ac6296573e97b7ded8e
MichaelMini <mcho911@gmail.com>
2017-02-03 03:08:41 +0000

    Login/out checks user and password. Only 1 user signup

--- server.js
+++ server.js
@@ -31,6 +31,19 @@ app.use(cookieSession({
   keys: ['123'],
   maxAge: 24 * 60 * 60 * 1000
 }))
+app.use(function(req, res, next){
+  req.user = req.session.user_id;
+  next();
+});
+
+//
+// app.use('/*?', (req, res, next) => {
+//   if (req.user) {
+//     next();
+//   } else {
+//     res.status(401).send('You must <a href="/login">Sign In</a> before you can enter this page. <br><br>Or you can <a href="/register">Register Here</a>');
+//   }
+// });
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,35 +58,82 @@ app.use(express.static("public"));
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
 
+// Set Cookie Username & Login Handler
+app.post('/login', (req, res) => {
+  // select * from users where user_name = 'michael';
+  knex.select().from('users').where('user_name', req.body.name).then(function (results) {
+    // console.log('user results: ', results)
+    if (results.length !== 1 || results[0].password !== req.body.password) {
+      res.send('Please <a href="/register">Sign-in</a> with a correct user name and password.\n Or you can <a href="/register">Register here</a> for a new account.', 401);
+      return
+    } else {
+      req.session.user_id = results[0].id;
+      res.redirect('/');
+    }
+  }).catch(function (err) {
+    res.status(500).send("oh crap.  see whatever...");
+  });
+});
+
+// Clear Cookie Logout
+app.post('/logout', (req, res) => {
+  req.session.user_id = '';
+  res.redirect('/register');
+});
+
 // Register page
 app.get("/register", (req, res) => {
-  console.log("user_id", req.session.user_id);
+  if (req.user) {
+    res.redirect('/');
+  }
+  console.log("user_id", req.user);
   res.render("register");
 });
 
 app.post("/register", (req, res) => {
   const user_name = req.body.name;
   const password = req.body.password;
-  knex('users')
-    .insert({'user_name': user_name, 'password': password})
-    .returning('id')
-    .asCallback((err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("oh crap.  see server log.");
+  knex.select().from('users').where('user_name', req.body.name)
+  .then((results) => {
+    if(results.length === 0) {
+      return knex('users')
+      .insert({'user_name': user_name, 'password': password})
+      .returning('id');
+    } else {
+      // if the users exists already then inform.
+      console.log('user already exists');
+      // alert("Hello! I am an alert box!");
+      res.redirect("register");
+    }
+  })
+  .then((results) => {
+    console.log("hopefully the new userid is in this: ", results);
+    if (results.length !== 1) {
+      console.log("what the hell is with this non-length-1 result: ", results);
+      res.status(500).send("oh crap.  see server log.");
         return;
       }
-      console.log("hopefully the new userid is in this: ", result);
-      if (result.length !== 1) {
-        console.log("what the hell is with this non-length-1 result: ", result);
-        res.status(500).send("oh crap.  see server log.");
-        return;
-      }
-      req.session.user_id = result[0];
+      req.session.user_id = results[0];
       res.redirect("register");  // TODO: better redirect
-    });
+  })
+  .catch((err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("oh crap.  see server log.");
+      return;
+    }
+  });
 });
 
+// knex.select().from('users').where('user_name', 'michael').asCallback((error, results) => {
+//   if(results.length === 0) {
+//     knex('users')
+//     .insert({'user_name': user_name, 'password': password})
+//     .returning('id').asCallback((error, results) => {
+//       // user should be inserted by now
+//     })
+//   }
+// });
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
