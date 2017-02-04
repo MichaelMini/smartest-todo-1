3512610cea16ca034c1891651a0366b5366dc2e8
MichaelMini <mcho911@gmail.com>
2017-02-04 06:31:45 +0000

    added toggle options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,10 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
+  // toggle options
+  // $('.dropdown-toggle').dropdown();
+  $('#divNewNotifications li > a').click(function(){
+    $('#text').text($(this).html());
+  });
 });
